25796160b7a92f5362833335173dfb4c3a5b6900
Sun <sunqianquan@adeaz.com>
2015-06-21 13:47:07 +0800

    菜单

---
+++ App/src/com/ad/block/view/ModeSelcetActivity.java
@@ -0,0 +1,18 @@
+package com.ad.block.view;
+
+import com.ad.block.R;
+import com.ad.block.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ModeSelcetActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mode_selcet);
+	}
+}
