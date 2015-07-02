8bf9c14c2eedc8a7955eb578b18279717c0d2020
Smile <lijianhy1990@gmail.com>
2015-07-02 23:40:06 +0800

    update gui anim

--- App/src/cn/adblock/view/GuiActivity.java
+++ App/src/cn/adblock/view/GuiActivity.java
@@ -3,15 +3,25 @@ package cn.adblock.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import cn.adblock.R;
@@ -34,6 +44,7 @@ public class GuiActivity extends BaseActivity implements OnPageChangeListener, O
 	private View[] dots;
 	private ViewPagerAdapter vpAdapter;
 	private List<View> views;
+	private View[] texts = new View[4];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,14 +60,20 @@ public class GuiActivity extends BaseActivity implements OnPageChangeListener, O
 		}
 		LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.MATCH_PARENT);
-		for (int i = 0; i < pics.length; i++) {
+		for (int i = 0; i < pics.length - 1; i++) {
 			ImageView iv = new ImageView(this);
 			iv.setId(IMG_RID+i);
 			iv.setLayoutParams(lp);
 			iv.setImageResource(pics[i]);
-			iv.setOnClickListener(this);
 			views.add(iv);
 		}
+		View view = LayoutInflater.from(this).inflate(R.layout.view_gui_start, null);
+		view.findViewById(R.id.vgs_view_start).setOnClickListener(this);
+		texts[0] = view.findViewById(R.id.vgs_img_one);
+		texts[1] = view.findViewById(R.id.vgs_img_two);
+		texts[2] = view.findViewById(R.id.vgs_img_three);
+		texts[3] = view.findViewById(R.id.vgs_img_four);
+		views.add(view);
 		vpAdapter = new ViewPagerAdapter(views);
 		viewPager.setAdapter(vpAdapter);
 		viewPager.setOnPageChangeListener(this);
@@ -82,12 +99,38 @@ public class GuiActivity extends BaseActivity implements OnPageChangeListener, O
 
 	@Override
 	public void onClick(View v) {
-		final int index = v.getId() - IMG_RID;
-		if (index == 2) {
-			startActivity(new Intent(this, MainActivity.class));
-			finish();
-		} else {
-			viewPager.setCurrentItem(index+1, true);
+		if (v.getId() == R.id.vgs_view_start) {
+			for (int i = 0; i < texts.length; i++) {
+				ObjectAnimator oa = ObjectAnimator.ofFloat(texts[i], "haha", 1f,1.5f,1f);
+				oa.addUpdateListener(new ScaleAnimatorListener(texts[i]));
+				oa.setStartDelay(i*100);
+				oa.setDuration(300);
+				if (i == texts.length - 1) {
+					oa.addListener(new AnimatorListenerAdapter(){
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							super.onAnimationEnd(animation);
+							startActivity(new Intent(GuiActivity.this, MainActivity.class));
+							finish();
+						}
+					});
+				}
+				oa.start();
+			}
+		}
+	}
+	
+	class ScaleAnimatorListener implements AnimatorUpdateListener{
+		View view;
+		public ScaleAnimatorListener(View v) {
+			view = v;
+		}
+		
+		@Override
+		public void onAnimationUpdate(ValueAnimator animation) {
+			float value = (float) animation.getAnimatedValue();
+			view.setScaleX(value);
+			view.setScaleY(value);
 		}
 	}
 
